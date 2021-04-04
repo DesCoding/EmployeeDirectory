86ba970e84db7f5b68d468f59168e75ea14c8e27
Des <d.stout31@yahoo.com>
2021-04-04 15:14:18 -0500

    added nav bar component, linked navbar in app.js, added nav info from react bootstrap, updated css to begin formatting the table

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import EmployeeTable from "./components/EmployeeTable";
+import Navbar from "react-bootstrap/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <EmployeeTable />
     </div>
   );
