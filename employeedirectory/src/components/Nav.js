3c7c70d34ecbd87d680214b66623c6648811d525
Des <d.stout31@yahoo.com>
2021-04-11 11:00:42 -0500

    removed extra search form

--- employeedirectory/src/components/Nav.js
+++ employeedirectory/src/components/Nav.js
@@ -9,18 +9,6 @@ function Nav(props) {
   return (
     <Navbar className="bg-light justify-content-between">
       <Form inline>
-        <InputGroup>
-          <InputGroup.Prepend>
-            <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
-          </InputGroup.Prepend>
-          <FormControl
-            placeholder="Username"
-            aria-label="Username"
-            aria-describedby="basic-addon1"
-          />
-        </InputGroup>
-      </Form>
-      <Form inline>
         <FormControl
           type="text"
           placeholder="Search"
